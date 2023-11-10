2aae54aca5dd973a82cff284be2211d31ae03862
Daniel Lee <jhoon8903@gmail.com>
2023-11-11 01:58:45 +0900

    Update

---
+++ Linq.cs
@@ -0,0 +1,9 @@
+namespace CsharpBasic;
+
+public class Linq
+{
+    public static void LINQ()
+    {
+        List<int> numbers = new List<int> { 1, 2, 2, 3, 4, 5, 5, 6};
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,6 @@ public static class Program
 {
     private static void Main()
     {
-         TicTacToe.StartTicTacToe();
+        Linq.LINQ();
     }
 }
